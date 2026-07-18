5bab6b0d441fed35154231fbe28cd77d30a5b498
Agnelo <km3corridas@gmail.com>
2026-07-18 20:11:36 +0000

    Pace Baixo: logomarca +50% (pedido do Paulo 18/07)

--- pacebaixo/pdfgen.js
+++ pacebaixo/pdfgen.js
@@ -113,7 +113,7 @@
     var titX = mx;
     if (opts.logo) {
       try {
-        var lgH = 10.4, lgW = lgH * 659 / 383;   // proporcao do PNG (659x383), +15% (pedido do Paulo 18/07)
+        var lgH = 15.6, lgW = lgH * 659 / 383;   // proporcao do PNG (659x383), +50% sobre a base (pedido do Paulo 18/07)
         doc.addImage(opts.logo, 'PNG', mx, y - 6.5, lgW, lgH);
         titX = mx + lgW + 4;
       } catch (e) { titX = mx; }
